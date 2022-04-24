f333315a3e7288207e17942b8a3687675fe725ac
Fatih Çalış <93558245+fatihcaliss@users.noreply.github.com>
2022-04-24 22:28:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+let enterButton = document.getElementById("enterButton");
+let againButton = document.getElementById("againButton");
+let output = document.getElementById("outputText");
+let letters = document.getElementById("flip_letters");
+
+let randomNumber = Math.floor(Math.random() * 51);
+
+let clap = new Audio("/clapsound.wav")
+let wrongsound = new Audio("/wrongsound.wav")
+
+function checkNumber() {
+
+    let input = document.getElementById("userInput").value;
+    
+    if(input == randomNumber){
+      output.innerHTML="You Guessed Right" + "," + "it was" + " " + randomNumber;
+      output.style.color="green";
+      clap.play();
+      let spans = document.querySelectorAll("span")
+      for (let i of spans) {
+          i.style.color="green";
+      }  
+
+    }
+    else if(input > randomNumber && input < 100){
+        output.innerHTML="You guessed too high";
+        wrongsound.play();
+    }
+    else if(input < randomNumber && input > 1){
+        output.innerHTML="You guessed too low";
+        wrongsound.play();
+    }
+    else if(input < 1){
+        output.innerHTML="Higher, it has to be between 1 and 100";
+        wrongsound.play();
+    }
+    else if(isNaN(input)){
+        output.innerHTML="That's not a number!";
+        wrongsound.play();
+    }
+    else {
+        output.innerHTML="Lower, it has to be between 1 and 50";
+        wrongsound.play();
+    }
+}
+
+enterButton.addEventListener("click", checkNumber);
+againButton.addEventListener("click", function(){
+    location.reload();
+});
